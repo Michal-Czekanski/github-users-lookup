1bb60c049e637d4a5d360932da8a6c2e4cd90c08
Michal-Czekanski <michal.cz2202@gmail.com>
2022-05-14 23:28:36 +0200

    remove autogenerated content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders "Hello World!"', () => {
+  render(<h1>Hello World!</h1>);
+  const linkElement = screen.getByText("Hello World!");
   expect(linkElement).toBeInTheDocument();
 });
